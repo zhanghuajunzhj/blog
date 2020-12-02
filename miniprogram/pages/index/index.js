ffce0457742edf86aeea869c0cba8febfad1f0a2
zhanghuajunzhj <zhanghj_zhj@163.com>
2020-12-02 21:09:23 +0800

    first commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,149 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    active: 1,
+    goods:"",
+    feed: [],
+    feed_length: 0
+  },
+
+  onLoad: function(event) {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+    // 创建数据库实例
+    const db = wx.cloud.database()
+    // this.getData();
+    let that = this;
+    db.collection('feed_data').get({
+      success(res) {
+        var feed_data = res.data;
+        console.log(feed_data);
+        that.setData({
+          feed:feed_data,
+          feed_length: feed_data.length
+        });
+      }
+    })
+    wx.request({
+      url: 'https://api.juejin.cn/recommend_api/v1/article/recommend_all_feed', //仅为示例，并非真实的接口地址
+      method:"POST",
+      data:{
+        client_type: 2608,
+        cursor: "0",
+        id_type: 2,
+        limit: 20,
+        sort_type: 200
+      },
+      header: {
+        "content-type": "application/json; charset=utf-8" // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
+  },
+
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = `my-image${filePath.match(/\.[^.]+?$/)[0]}`
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+})
